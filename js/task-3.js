47e6036060885eef03951bca0abd35924853f9cc
Dimass1515 <di.tsvigun@gmail.com>
2024-02-26 12:10:40 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,20 @@
 class StringBuilder {
     #value
     constructor(initialValue) {
-        this.#value = [initialValue]
+        this.#value = initialValue
     }
     getValue() {
         return this.#value;
     }
     padEnd(str) {
-        this.#value.push(str);
+        this.#value += str;
     }
     padStart(str) {
-        this.#value.unshift(str);
+        this.#value = str + this.#value;
     }
     padBoth(str) {
-        this.#value.unshift(str)
-        this.#value.push(str);
+        this.#value.padStart(str)
+        this.#value.padEnd(str);
     }
 
 }
